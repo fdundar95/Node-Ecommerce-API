dd259991c15f8efbd7b01e593e1865f075c12cd6
fdundar95 <79167732+fdundar95@users.noreply.github.com>
2023-09-30 01:28:28 +0300

    Refactor for readability

--- utils/checkPermissions.js
+++ utils/checkPermissions.js
@@ -1,8 +1,11 @@
 const CustomError = require('../errors');
 
 const checkPermissions = (requestUser, resourceUserId) => {
-    if (requestUser.role === 'admin') return;
-    if (requestUser.userId === resourceUserId.toString()) return;
+    const isAdmin = requestUser.role === 'admin';
+    const isAuthorized = requestUser.userId === resourceUserId.toString();
+    if (isAdmin || isAuthorized) {
+        return;
+    }
 
     throw new CustomError.UnauthorizedError('Not authorized to access this route');
 };
