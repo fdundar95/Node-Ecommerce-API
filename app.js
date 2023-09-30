67cc431c155f23d3d9ae17e9d5b9f326a003d31a
fdundar95 <79167732+fdundar95@users.noreply.github.com>
2023-09-30 03:13:51 +0300

    Add security packages & remove unnecessary code

--- app.js
+++ app.js
@@ -2,12 +2,23 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 require('express-async-errors');
-const morgan = require('morgan');
 const cookieParser = require('cookie-parser');
 const fileUpload = require('express-fileupload');
+const rateLimiter = require('express-rate-limit');
+const helmet = require('helmet');
+const xss = require('xss-clean');
+const cors = require('cors');
+const mongoSanitize = require('express-mongo-sanitize');
 
 const connectDB = require('./db/connect');
-app.use(morgan('dev'));
+
+app.set('trust proxy', 1);
+
+app.use(cors());
+app.use(rateLimiter({ windowMs: 15 * 60 * 1000, max: 60 }));
+app.use(helmet());
+app.use(xss());
+app.use(mongoSanitize());
 app.use(express.json());
 app.use(cookieParser(process.env.JWT_SECRET));
 app.use(express.static('./public'));
@@ -24,13 +35,7 @@ const { authenticateUser } = require('./middleware/authentication');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
-app.get('/', (req, res) => {
-    res.send('E-commerce API');
-});
-app.get('/api/v1', (req, res) => {
-    console.log(req.signedCookies);
-    res.send('E-commerce API');
-});
+app.use(express.static('./public'));
 app.use('/api/v1/auth', authRouter);
 app.use('/api/v1/users', authenticateUser, userRouter);
 app.use('/api/v1/products', productRouter);
